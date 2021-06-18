7a72b4f0ed91bee173b9ee05731c5af8729ca17b
Ne-Lexa <alexey@nelexa.ru>
2021-06-18 21:47:01 +0300

    bump version

--- src/Crossplane.php
+++ src/Crossplane.php
@@ -12,7 +12,7 @@ use Nelexa\NginxParser\Ext\LuaBlockPlugin;
  */
 class Crossplane
 {
-    public const VERSION = '1.0.0-dev';
+    public const VERSION = '1.0.0';
 
     private const DEFAULT_ENABLED_EXTENSION = [LuaBlockPlugin::class];
 
